5883290e6fa584df9a4946e8f7bbd275c32b8927
dimpledhawan <dhawan.dimple21@gmail.com>
2018-09-17 08:55:44 -0400

    fixed parse and ran test

--- tutorial/spiders/quotes_spider.py
+++ tutorial/spiders/quotes_spider.py
@@ -3,16 +3,12 @@ import scrapy
 class QuotesSpider(scrapy.Spider):
     name= "quotes"
 
-    def start_requests(self):
-        urls = [
+    start_urls = [
             'http://quotes.toscrape.com/page/1/',
             'http://quotes.toscrape.com/page/2/',
         ]
 
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-    
-    def parse:
+    def parse(self, response):
         page = response.url.split("/")[-2]
         filename = 'quotes-%s.html' % page
         with open(filename, 'wb') as f:
